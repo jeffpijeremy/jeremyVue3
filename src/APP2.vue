6f2ca20b0c8b6614b5338e502838fc1950520baa
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2022-07-01 20:17:10 +0800

    Initial commit

---
+++ src/APP2.vue
@@ -0,0 +1,14 @@
+<template>
+    <HelloWorld msg="Hello Vue 123 + Jeremy" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App2',
+    components: {
+        HelloWorld
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import APP2 from './App2.vue'
+import './index.css'
+
+createApp(App).mount('#app')
+createApp(APP2).mount('#demo')
