e9c802705f14d052087220c844e727134a3c2298
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2022-07-01 20:52:52 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import APP2 from './App2.vue'
+import App2 from './App2.vue'
+import vm from './vm.vue'
 import './index.css'
 
 createApp(App).mount('#app')
-createApp(APP2).mount('#demo')
+createApp(App2).mount('#demo')
+createApp(vm).mount('#vm')
