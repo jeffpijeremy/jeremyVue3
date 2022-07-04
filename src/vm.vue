121827109638aa27d4494aece4a33665c946aa6d
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2022-07-04 09:08:24 +0800

    增加函示

--- src/vm.vue
+++ src/vm.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
-    {{name}} 好棒棒!
+        {{name}} 好棒棒!
+        <p></p>
+        價格 : {{price}}
+        <p></p>
+        數量 : {{quantity}}
+        <p></p>
+        總價 : {{subtotal()}}
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            name: '1234'
+            name: '1234',
+            price : 50,
+            quantity : 5
+        }
+    },
+    methods: {
+        subtotal:function(){
+            return this.price * this.quantity;
         }
     }
 }
